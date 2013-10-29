8d172be90c739efe3c5bf1d20ad0a6f07e1258b7
Ryan Seddon <seddon.ryan@gmail.com>
2013-10-29 14:43:44 +1100

    Make compiler generate start generator syntax
    * Remove escodegen option to not do function foo*(){} syntax
    * Add package.json to get deps
    * Do gitignore on node_modules

--- compiler/main.js
+++ compiler/main.js
@@ -12,6 +12,4 @@ var node = esprima.parse(src, { loc: true, range: true });
 node = compiler.compile(node, src);
 
 //console.log(prettyprint.pretty(node));
-console.log(escodegen.generate(node, {
-    moz: { starlessGenerator: true }
-}));
+console.log(escodegen.generate(node));
